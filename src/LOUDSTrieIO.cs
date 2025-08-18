79c5b73e926f0082febc68fa33c0058a040f6c1b
tkxp200 <kannzakikousukerecter@gmail.com>
2025-08-18 19:26:22 +0900

    update: LOUDSIO

--- src/LOUDSTrieIO.cs
+++ src/LOUDSTrieIO.cs
@@ -6,7 +6,7 @@ public static class LOUDSTrieIO
 {
     public static async ValueTask SaveTrieAsync<T>(LOUDSTrie<T> trie, string filePath)
     {
-        using (var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None, 4096, true))
+        await using (var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None, 4096, true))
         {
             await MemoryPackSerializer.SerializeAsync(fs, trie);
         }
@@ -14,7 +14,7 @@ public static class LOUDSTrieIO
 
     public static async ValueTask<LOUDSTrie<T>> LoadTrieAsync<T>(string filePath)
     {
-        using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true))
+        await using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true))
         {
             var result = await MemoryPackSerializer.DeserializeAsync<LOUDSTrie<T>>(fs);
             if (result is null) throw new InvalidDataException("Failed to load LOUDSTrie.");
